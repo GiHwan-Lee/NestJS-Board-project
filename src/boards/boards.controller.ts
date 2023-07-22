2ee9976b85a051e1eda2e592b35039e135958332
Gi Hwan <ass302@naver.com>
2023-07-22 19:08:33 +0900

    -Create board model file and implement code
    -Implement POST API for creating a board-
    
    -Create DTO for board module
    
    -Implemented auto end-of-line handling with Prettier to resolve
    cross-platform CRLF/LF issues

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './boards.model';
+import { CreateBoardDto } from './dto/create-board-dto';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
   @Get('/')
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boardService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardService.createBoard(createBoardDto);
   }
 }
