91004d527aa4fa0125a0bff20a184da53439865b
Gi Hwan <ass302@naver.com>
2023-08-05 20:45:12 +0900

    -Ensure username uniqueness during signup by modifying entity and repository code -Install and apply bcrypt library for password encryption during signup -Add login functionality by creating signIn API and writing corresponding controller and service code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credential-dto';
 
@@ -8,5 +9,16 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.findOneBy({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'logIn success';
+    } else {
+      throw new UnauthorizedException('logIn failed');
+    }
   }
 }
