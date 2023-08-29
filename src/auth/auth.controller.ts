b32196d8dcf346ae01f3b8780da9a912b005615f
Gi Hwan <ass302@naver.com>
2023-08-29 23:22:41 +0900

    Added comments on auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,10 +2,12 @@ import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential-dto';
 
+// 'auth' 경로에 대한 컨트롤러를 정의합니다.
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {} // auth 서비스 주입
 
+  // 사용자 어플리케이션 가입
   @Post('/signup')
   signUp(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
@@ -13,6 +15,7 @@ export class AuthController {
     return this.authService.signUp(authCredentialsDto);
   }
 
+  // 인증된 사용자 로그인
   @Post('/signin')
   singIn(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
